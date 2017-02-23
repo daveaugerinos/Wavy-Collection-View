480443ab0ce98bb0adcc34623d2316d9728b9ea9
daveaugerinos <dave.augerinos@gmail.com>
2017-02-23 08:57:14 -0800

    Initial setup

---
+++ Wavy-Collection-View/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Wavy-Collection-View
+//
+//  Created by Dave Augerinos on 2017-02-23.
+//  Copyright © 2017 Dave Augerinos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Wavy-Collection-View/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Wavy-Collection-View
+//
+//  Created by Dave Augerinos on 2017-02-23.
+//  Copyright © 2017 Dave Augerinos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
